953a1415954b86c7a10195e3aebe9de98369de4b
Mark Smit <m.smit@hostingondemand.nl>
2016-10-19 11:05:32 +0200

    more apropriate language

--- samples/saveProducts.php
+++ samples/saveProducts.php
@@ -17,20 +17,20 @@ class Example
         $session = $api->createSession(API_KEY);
 
         $category1=new ProductCategory($session);
-        $category1->name="hoi";
+        $category1->name="Category 1";
         $category1->id=1;
         $category1->save();
 
 
         $category2=new ProductCategory($session);
-        $category2->name="doei";
+        $category2->name="Category 2";
         $category2->id=2;
         $category2->save();
 
 //add/update a product
         $product1 = new Product($session);
         $product1->id = 1;
-        $product1->name = "Sword art online";
+        $product1->name = "Product 1";
         $product1->quantity = 100;
         $product1->price = 10.50;
         $product1->url = "http://producturl.nl";
@@ -49,7 +49,7 @@ class Example
         //add/update a product
         $product2 = new Product($session);
         $product2->id = 2;
-        $product2->name = "productje";
+        $product2->name = "Product 2";
         $product2->quantity = 100;
         $product2->price = 10.50;
         $product2->url = "http://producturl.nl";
@@ -67,7 +67,7 @@ class Example
         //add/update a product
         $product3 = new Product($session);
         $product3->id = 3;
-        $product3->name = "en nog 1";
+        $product3->name = "Product 3";
         $product3->quantity = 100;
         $product3->price = 10.50;
         $product3->url = "http://producturl.nl";
